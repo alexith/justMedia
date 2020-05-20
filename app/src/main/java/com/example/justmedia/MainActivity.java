f2900747d5462386c04045e4d1dcd5c078f66ad0
alexith <akshay.cy2@gmail.com>
2020-05-20 14:51:59 +0530

    init commit of media; fade in general, here in this topic

--- app/src/main/java/com/example/justmedia/MainActivity.java
+++ app/src/main/java/com/example/justmedia/MainActivity.java
@@ -10,13 +10,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void fade(View view) {
 
-        ImageView mojoSmall = findViewById(R.id.mojoSmall);
-
-        mojoSmall.animate().alpha(0f).setDuration(2000);
-
         ImageView mojoBig = findViewById(R.id.mojoBig);
 
-        mojoBig.animate().alpha(-0f).setDuration(2000);
+        mojoBig.animate().alpha(0f).setDuration(2000);
 
     }
 
